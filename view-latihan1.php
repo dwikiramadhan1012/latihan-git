349a8d1e2031a51c61d85290e061d97c7cba80a3
dwiki <dwikiramadhann1@gmail.com>
2021-09-22 23:36:54 +0700

    first commit

---
+++ view-latihan1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+ <title>Latihan 1</title>
+</head>
+<body>
+    <center><img src="https://images.hdqwalls.com/wallpapers/funny-ninja-on-diet-qu.jpg"width=50% height="50%"></center>
+
+ <center><b>Halo Kawan Dari pada anda nonton wibu,ngebadut,main epep mening belajar web programming aja !!!!</center><br>
+ <center>Nilai 1 = <?= $nilai1; ?></center> <br>
+ <center>Nilai 2 = <?= $nilai2; ?> </center><br>
+ <center>ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+<style>
+    body{
+        background-color: darkgrey;
+    border
+
+    }
+</style>
+</body>
+
+</html>
