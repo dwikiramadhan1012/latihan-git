5dab326cb4e79a33cb80e2ec723dab9f72ca0a35
dwiki <dwikiramadhann1@gmail.com>
2021-10-12 13:32:16 +0700

    tugas pertemuan 4

---
+++ v_about.php
@@ -0,0 +1,20 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="circle">
+        <li>Nama : Dwiki Ramadhan</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="circle">
+        <li> Jalan Bambu Hitam No 90 Gang Mild</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="circle">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="circle">
+        <li>Bulutangkis</li>
+    </ul
+</section>
---
+++ v_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Web Programing Template Sederhana Dengan CI</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<hgroup>
+				<h1>Info Web Programing</h1>
+				<h3>Membuat Template Sederhana Dengan CI </h3>
+			</hgroup>
+			<nav>
+				<ul>
+					<li><a href="<?php echo base_url() . 'Web'	?>">Home</a></li>
+					<li><a href="<?php echo base_url() . 'web/about'	?>">About</a></li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</header>
